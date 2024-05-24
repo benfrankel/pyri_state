647324230d71934a1bcdbf7d6caa0c9e406f6a80
Ben Frankel <ben.frankel7@gmail.com>
2024-05-24 16:51:58 -0700

    Add split state implementation

---
+++ src/extra/split.rs
@@ -0,0 +1,25 @@
+/// Define your own split state type by newtyping SplitState:
+///
+/// ```rust
+/// use pyri_state::prelude::*;
+/// use pyri_state::extra::split::SplitState;
+/// use pyri_state::add_to_split_state;
+///
+/// #[derive(State, Clone, PartialEq, Eq)]
+/// pub struct MyState(pub SplitState);
+///
+/// add_to_split_state!(MyState, Foo, Bar);
+/// add_to_split_state!(MyState, Quux);
+/// ```
+
+pub type SplitState = &'static str;
+
+#[macro_export]
+macro_rules! add_to_split_state {
+    ($ty:ident, $($val:ident),* $(,)*) => {
+        #[allow(non_upper_case_globals)]
+        impl $ty {
+            $(const $val: $ty = $ty(stringify!($val));)*
+        }
+    };
+}
