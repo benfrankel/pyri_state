477cd32c83b3e08fa84fbb7b41825566a7a06b5c
Ben Frankel <ben.frankel7@gmail.com>
2024-05-25 13:38:03 -0700

    Fix modular_configuration example

--- examples/modular_configuration.rs
+++ examples/modular_configuration.rs
@@ -12,51 +12,59 @@ use pyri_state::{
 fn main() {
     let mut app = App::new();
     app.add_plugins(PyriStatePlugin)
-        .add_state_::<MyDerivedState>()
         .add_state_::<MyRawState>()
+        .add_state_::<MyDerivedState>()
         .add_state_::<MyCustomState>();
 }
 
-#[derive(State, PartialEq, Eq, Clone, Hash, Debug)]
-#[state(
-    // Disable default plugins: detect_change, flush_event, apply_flush.
-    no_defaults,
-    // Trigger a flush on any state change.
-    detect_change,
-    // Send an event on flush.
-    flush_event,
-    // Include a BevyState wrapper (see ecosystem_compatibility example).
-    bevy_state,
-    // Clone the next state into the current state on flush.
-    apply_flush,
-    // Run this state's on flush systems after the listed states.
-    after(FooState, BarState<i32>),
-    // Run this state's on flush systems before the listed states.
-    before(QuuxState)
-)]
-struct MyDerivedState;
-
-// Clone, PartialEq, and Eq can be omitted if they won't be needed.
+// You can derive State on its own if no other traits are required.
 #[derive(State)]
 #[state(no_defaults)]
 struct MyRawState;
 
-// TODO: Explain this in comments
+#[derive(State, PartialEq, Eq, Clone, Hash, Debug)]
+// State plugins are fully customizable per state type:
+#[state(
+    // Disable default plugins: detect_change, flush_event, apply_flush.
+    no_defaults,
+    // Trigger a flush on any state change (requires PartialEq, Eq).
+    detect_change,
+    // Send an event on flush (requires Clone).
+    flush_event,
+    // Include a BevyState wrapper (requires Clone, PartialEq, Eq, Hash, Debug).
+    // See ecosystem_compatibility example for more information.
+    bevy_state,
+    // Clone the next state into the current state on flush (requires Clone).
+    apply_flush,
+    // Run this state's on flush systems after the listed states.
+    after(MyRawState),
+    // Run this state's on flush systems before the listed states.
+    before(MyCustomState, DummyState)
+)]
+struct MyDerivedState;
+
+// Deriving RawState instead of State allows you to impl ConfigureState yourself,
+// allowing for fully custom state configuration (see below).
 #[derive(RawState, Clone, PartialEq, Eq, Hash, Debug)]
 struct MyCustomState;
 
+// This will be called from `app.add_state_`, `init_state_`, and `insert_state_`.
 impl ConfigureState for MyCustomState {
     fn configure(app: &mut App) {
         app.add_plugins((
             ResolveStatePlugin::<Self>::default()
+                .after::<MyRawState>()
                 .after::<MyDerivedState>()
-                .before::<MyRawState>(),
+                .before::<DummyState>(),
             DetectChangePlugin::<Self>::default(),
             FlushEventPlugin::<Self>::default(),
             BevyStatePlugin::<Self>::default(),
             ApplyFlushPlugin::<Self>::default(),
         ));
 
-        // ... some more custom configuration ...
+        // ... some more custom configuration on app ...
     }
 }
+
+#[derive(RawState)]
+struct DummyState;
