c20302b53b701bc8ffb210899ed6f979254f17d3
Ben Frankel <ben.frankel7@gmail.com>
2024-07-13 23:45:51 -0700

    Fix 1-frame delay with `BevyState` wrapper

--- src/extra/bevy_state.rs
+++ src/extra/bevy_state.rs
@@ -48,9 +48,10 @@ pub use app::*;
 mod app {
     use std::{fmt::Debug, hash::Hash, marker::PhantomData};
 
-    use bevy_app::{App, Plugin};
+    use bevy_app::{App, Plugin, PreUpdate};
+    use bevy_state::prelude as bevy;
 
-    use crate::{schedule::StateFlush, state::StateMut};
+    use crate::state::StateMut;
 
     use super::{schedule_bevy_state, BevyState};
 
@@ -62,8 +63,8 @@ mod app {
 
     impl<S: StateMut + Clone + PartialEq + Eq + Hash + Debug> Plugin for BevyStatePlugin<S> {
         fn build(&self, app: &mut App) {
-            bevy_state::app::AppExtStates::init_state::<BevyState<S>>(app);
-            schedule_bevy_state::<S>(app.get_schedule_mut(StateFlush).unwrap());
+            bevy::AppExtStates::init_state::<BevyState<S>>(app);
+            schedule_bevy_state::<S>(app.get_schedule_mut(PreUpdate).unwrap());
         }
     }
 
@@ -76,22 +77,18 @@ mod app {
 
 use std::{fmt::Debug, hash::Hash};
 
-use bevy_ecs::{
-    schedule::{IntoSystemConfigs, Schedule},
-    system::{Res, ResMut},
-};
-use bevy_state::state::{NextState, States};
+use bevy_ecs::{schedule::Schedule, system::ResMut};
+use bevy_state::prelude as bevy;
 
 use crate::{
-    access::{NextMut, NextRef},
-    schedule::ResolveStateSet,
+    access::NextMut,
     state::{State, StateMut},
 };
 
 /// A wrapper around the [`State`] type `S` for compatibility with the Bevy ecosystem.
 ///
 /// Any change to `S` will propagate to `BevyState<S>`, and vice versa.
-#[derive(States, Clone, PartialEq, Eq, Hash, Debug)]
+#[derive(bevy::States, Clone, PartialEq, Eq, Hash, Debug)]
 pub struct BevyState<S: State + Clone + PartialEq + Eq + Hash + Debug>(
     /// The wrapped state value, or `None` if disabled.
     pub Option<S>,
@@ -109,22 +106,17 @@ impl<S: State + Clone + PartialEq + Eq + Hash + Debug> Default for BevyState<S> 
 pub fn schedule_bevy_state<S: State + StateMut + Clone + PartialEq + Eq + Hash + Debug>(
     schedule: &mut Schedule,
 ) {
-    let update_bevy_state =
-        |pyri_state: NextRef<S>, mut bevy_state: ResMut<NextState<BevyState<S>>>| {
-            if matches!(bevy_state.as_ref(), NextState::Unchanged) {
-                bevy_state.set(BevyState(pyri_state.get().cloned()));
+    let sync_states =
+        |mut pyri_state: NextMut<S>, mut bevy_state: ResMut<bevy::NextState<BevyState<S>>>| {
+            match bevy_state.as_ref() {
+                bevy::NextState::Pending(bevy_state) => {
+                    pyri_state.trigger().set(bevy_state.0.clone());
+                }
+                bevy::NextState::Unchanged => {
+                    bevy_state.set(BevyState(pyri_state.get().cloned()));
+                }
             }
         };
 
-    let update_pyri_state = |mut pyri_state: NextMut<S>,
-                             bevy_state: Res<NextState<BevyState<S>>>| {
-        if let NextState::Pending(bevy_state) = bevy_state.as_ref() {
-            pyri_state.trigger().set(bevy_state.0.clone());
-        }
-    };
-
-    schedule.add_systems((
-        update_pyri_state.in_set(ResolveStateSet::<S>::Compute),
-        update_bevy_state.in_set(ResolveStateSet::<S>::Flush),
-    ));
+    schedule.add_systems(sync_states);
 }
